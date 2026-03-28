613b180b326649823ad8c1d6b6c4181dd23ead25
medder001 <mee95@pitt.edu>
2026-03-27 22:11:31 -0400

    Modify test.js

--- test.js
+++ test.js
@@ -1,13 +1,11 @@
-const express = require('express'); // Import the express module
-const app = express();              // Create an Express application instance
-const PORT = 80;                  // Define the port number
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
-// Define a basic route for the root URL (/)
 app.get('/', (req, res) => {
-  res.send('Hello World!'); // Send a response back to the client
+    res.sendFile(__dirname + '/index.html');
 });
 
-// Start the server and listen for incoming requests
 app.listen(PORT, () => {
-  console.log(`Server is running on dynamiteschedules.com`); // Log a message to the console when the server starts
+    console.log(`Server is running on port ${PORT}`);
 });

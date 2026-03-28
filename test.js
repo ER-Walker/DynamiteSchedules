1ab6412beb784e1bdde4711451728f07e49b5348
medder001 <mee95@pitt.edu>
2026-03-27 21:48:44 -0400

    Create generic test.js file

---
+++ test.js
@@ -0,0 +1,13 @@
+const express = require('express'); // Import the express module
+const app = express();              // Create an Express application instance
+const PORT = 3000;                  // Define the port number
+
+// Define a basic route for the root URL (/)
+app.get('/', (req, res) => {
+  res.send('Hello World!'); // Send a response back to the client
+});
+
+// Start the server and listen for incoming requests
+app.listen(PORT, () => {
+  console.log(`Server is running on http://dynamiteschedules.com`); // Log a message to the console when the server starts
+});

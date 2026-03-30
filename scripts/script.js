01fbde3324b3b5ef47928f666282c7a919b6e703
medder001 <mee95@pitt.edu>
2026-03-30 13:42:34 -0400

    Fix file path name in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ const port = 3000;
 console.log('Server is running on port ' + port);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../pages/login.html'));
+    res.sendFile(__dirname + '/login.html');
 });
 
 app.listen(port, () => {

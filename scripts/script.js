25515e7f5328fb348a606b5f26a12288bd16b0c6
medder001 <mee95@pitt.edu>
2026-03-30 13:55:38 -0400

    New file path config for express

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import __dirname from 'path';
 
 const app = express();
 const port = 3000;
@@ -7,7 +6,7 @@ const port = 3000;
 console.log('Server is running on port ' + port);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../pages/login.html'));
+    res.sendFile(import.meta.dirname + '/../pages/login.html');
 });
 
 app.listen(port, () => {

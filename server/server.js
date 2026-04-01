e0b92c84f4c0c780397d0fa1069aedb3a7c75b33
ER-Walker <erwalker816@icloud.com>
2026-04-01 12:21:25 -0400

    Adjusted Server to redirect to resources upon anchor click

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import {dirname, join} from 'path';
 const app = express();
 const port = 3000;
 const __dirname = '/var/www/dynamiteschedules/DynamiteSchedules';
+let filePath = __dirname
 
 console.log('Server is running on port ' + port);
 
@@ -15,10 +16,18 @@ mongoose.connect(process.env.MONGO_URI)
 	.catch(err => console.error('Connection error: ' , err));
 
 app.get('/', (req, res) => {
-    const filePath = __dirname + '/pages/login.html';
+    filePath += '/pages/login.html';
     console.log('Serving file: ' + filePath);
     res.sendFile(filePath);
+    filePath = __dirname;
 });
+
+app.get('/resources', (req,res) => {
+    filePath += '/pages/resources.html';
+    console.log('Serving file: ' + __dirname + '/pages/resources.html');
+    res.sendFile(filePath);
+    filePath = __dirname;
+})
 
 app.listen(port, () => {
     console.log('Server is listening on port ' + port);
